0461a77b39f90a39527865377439e4051abcca14
AlanMV <mv.alan4806@gmail.com>
2026-04-16 23:00:22 -0600

    Add: function Suma_binaria

--- main.c
+++ main.c
@@ -23,8 +23,27 @@ DecodedFloat decode(float f){
 
 float suma_binaria(float a, float b){
     DecodedFloat da = decode(a);
-    
+    DecodedFloat db = decode(b);
 
+    while(da.exponente < db.exponente){
+        da.mantissa >>= 1;
+        da.exponente++;
+    }
+    while(db.exponente < db.exponente){
+        db.mantissa >>= 1;
+        db.exponente++;
+    }
 
+    uint32_t res_mantissa = da.mantissa + db.mantissa;
+    int32_t res_exponente = da.exponente;
 
+    if(res_exponente & 0x1000000){
+        res_mantissa >>= 1;
+        res_exponente++;
+    }
+
+    uint32_t res_reg = (da.signo << 31) | ((res_exponente + 127) << 23 | (res_mantissa & 0x7FFFFF));
+
+    return *(float*)&res_reg;
 }
+
